e613eea24f4da4e073cdce6d752a566d5ac4f7dc
Matt <mckaymatt@outlook.com>
2022-05-26 16:19:39 -0400

    Navbar & Sidebar in progress

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css'
 
 function App() {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
   
     return (
     <div>
