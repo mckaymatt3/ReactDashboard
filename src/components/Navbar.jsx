826da0403679fd40fec7940752f829a8950ff147
Matt <mckaymatt@outlook.com>
2022-05-26 16:36:03 -0400

    Navbar & Sidebar done before pages created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,14 +15,34 @@ const NavButton = ({ title, customFunc, icon, color, dotColor}) => (
     <TooltipComponent content={title} positon="BottomCenter">
         <button type="button" onClick={customFunc} style={{ color }} className="relative text-xl rounded-full p-3 hover:bg-light-gray">
             <span style={{ background: dotColor}} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-                {icon}
             </span>
+            {icon}
         </button>
     </TooltipComponent>
 )
 
 const Navbar = () => {
-    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext();
+    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+    
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+        
+        window.addEventListener('resize', handleResize)
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    useEffect(() => {
+        if(screenSize <= 900) {
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true)
+        }
+    }, [screenSize])
+    
+    
   
     return (
         <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,13 +8,13 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
 
-  // const handleCloseSideBar = () => {
-  //   if (activeMenu !== undefined && screenSize <= 900) {
-  //     setActiveMenu(false);
-  //   }
-  // };
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -24,7 +24,7 @@ const Sidebar = () => {
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
-            <Link to="/" onClick={() => setActiveMenu(false)} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+            <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
               <SiShopware /> <span>St@s</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
@@ -48,7 +48,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     // style={({ isActive }) => ({
                     //   backgroundColor: isActive ? currentColor : '',
                     // })}
